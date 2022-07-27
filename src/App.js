079a813b870f7f5ce7437f42ef7bbe7966917723
kobby08 <linusquam@gmail.com>
2022-07-27 22:39:56 +0000

    final commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { BsFillAlarmFill, BsPlusLg, BsXLg } from "react-icons/bs";
 
 function App() {
+  const url = "http://localhost:8000/tasks/";
+  const [input, setInput] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  const fetchTasks = (url) => {
+    fetch(url)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setTasks(data);
+      });
+  };
+
+  // submitting task
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const task = { name: input, completed: false };
+    fetch(url, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(task),
+    }).then(() => {
+      setInput("");
+      fetchTasks(url);
+    });
+  };
+
+  // deleting task
+  const handleDelete = (id) => {
+    fetch(url + id, { method: "DELETE" }).then(() => {
+      fetchTasks(url);
+    });
+  };
+
+  // toggle completed task
+
+  const toggleConplete = (task) => {
+    const obj = { name: task.name, completed: !task.completed };
+    fetch(url + task.id, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(obj),
+    }).then(() => {
+      fetchTasks(url);
+    });
+  };
+
+  useEffect(() => {
+    fetchTasks(url);
+  }, [url]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="app">
+      <div className="container">
+        <h1>
+          <BsFillAlarmFill /> Daily Task
+        </h1>
+
+        <form onSubmit={handleSubmit}>
+          <div className="form-input">
+            <BsPlusLg className="icon" />
+            <input
+              type="text"
+              required
+              placeholder="Enter a task"
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+            />
+          </div>
+        </form>
+
+        <div>
+          {tasks.map((task) => (
+            <div
+              key={task.id}
+              onDoubleClick={() => toggleConplete(task)}
+              className={`task-row ${task.completed ? "completed" : ""}`}
+            >
+              <p>{task.name}</p>
+              <BsXLg onClick={() => handleDelete(task.id)} className="icon" />
+            </div>
+          ))}
+        </div>
+
+        <p className="length">
+          {tasks < 1 ? "You have no tasks" : `Tasks: ${tasks.length}`}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
   );
 }
